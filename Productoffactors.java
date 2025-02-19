865c7af5e025da6065bf94bd3170909634892a13
sairam-14 <velpulasairam51@gmail.com>
2025-02-19 22:47:17 +0530

    Add files via upload

---
+++ Productoffactors.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Productoffactors 
+{
+	public static void main(String[] args) 
+	{
+	  Scanner sc = new Scanner(System.in);
+	  System.out.println("Enter the number");
+	  int num = sc.nextInt();
+	  int product=1;
+	  for(int i=1;i<=num;i++){
+		  if(num%i==0){
+			  product = product*i;
+		  }
+	  }
+	  System.out.println(product);
+		
+	}
+}

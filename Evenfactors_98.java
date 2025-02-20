920c56471d5f450545ac659c5932fc89c36ca833
sairam-14 <velpulasairam51@gmail.com>
2025-02-20 23:57:07 +0530

    Add files via upload

---
+++ Evenfactors_98.java
@@ -0,0 +1,13 @@
+class  Evenfactors_98
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Even Factors of 98");
+		for(int i =1;i<=98;i++){
+			if(98%i==0&&i%2==0){
+				System.out.println(i);
+			}
+		}
+
+	}
+}
